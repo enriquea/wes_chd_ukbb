89a3ffee67634760ff0cb3a1d80d953cf6fd9589
enriquea <enrique.audain@gmail.com>
2021-03-04 01:13:07 +0100

    fixed relatedness pipeline

--- sample_qc/relatedness_inference.py
+++ sample_qc/relatedness_inference.py
@@ -3,11 +3,10 @@ Compute relatedness estimates between individuals using a variant of the PC-Rela
 
 Usage example:
 
-python sample_relatedness --mt_path 'path/to/mt' \
-                          --ht_output_path 'path/to/hts' \
-                          --maf_threshold 0.05 \
-                          --ld_pruning \
-                          --write_to_file
+python relatedness_inference.py --mt_path 'path/to/mt' \
+                                --ht_output_path 'path/to/hts' \
+                                --prune_ld \
+                                --write_to_file
 """
 
 import argparse
@@ -23,8 +22,9 @@ hdfs_dir = 'hdfs://spark-master:9820'
 
 
 def main(args):
-    # Initializing Hail on cluster mode
-    hl.init()
+
+    # Init Hail
+    hl.init(default_reference=args.default_reference)
 
     # Read MatrixTable
     mt = hl.read_matrix_table(args.mt_input_path)
@@ -43,16 +43,10 @@ def main(args):
     # keep only GT entry field and force to evaluate expression
     mt = (mt
           .select_entries(mt.GT)
-          .checkpoint(f'{hdfs_dir}/tmp/mt_filtered_for_pc_relate_28092020.mt')
+          .checkpoint(f'{hdfs_dir}/tmp/chd_ukbb.filtered_high_confidence_variants.mt')
           )
 
-    if args.sample_to_keep is not None:
-        sample_table = hl.import_table(paths=args.sample_to_keep,
-                                       no_header=True)
-        sample_set = hl.literal(sample_table.aggregate(hl.agg.collect_as_set(sample_table.f0)))
-        mt = mt.filter_cols(sample_set.contains(mt.s), keep=True)
-
-    if args.ld_pruning:
+    if args.prune_ld:
         # LD pruning
         # Avoid filtering / missingness entries (genotypes) before run LP pruning
         # Zulip Hail support issue -> "BlockMatrix trouble when running pc_relate"
@@ -63,30 +57,38 @@ def main(args):
 
         logger.info(f'Pruning variants in LD from MT with {mt.count_rows()} variants...')
 
-        pruned_variant_table = hl.ld_prune(mt.GT,
-                                           r2=args.r2,
-                                           bp_window_size=args.bp_window_size,
-                                           memory_per_core=1024)
+        pruned_variant_table = hl.ld_prune(mt.GT, r2=args.r2)
 
         # Keep LD-pruned variants
-        mt = (mt
-              .filter_rows(hl.is_defined(pruned_variant_table[mt.locus, mt.alleles]), keep=True)
-              )
+        pruned_mt = (mt
+                     .filter_rows(hl.is_defined(pruned_variant_table[mt.row_key]), keep=True)
+                     )
+        pruned_mt.write(f'{hdfs_dir}/tmp/chd_ukbb.ld_pruned.mt', overwrite=args.overwrite)
+
+    pruned_mt = hl.read_matrix_table(f'{hdfs_dir}/tmp/chd_ukbb.ld_pruned.mt')
+    v, s = pruned_mt.count()
+    logger.info(f'{s} samples, {v} variants found in LD-pruned MT')
 
     # run pc_relate method...compute all stats
-    logger.info(f'Running pc_relate method with {args.n_pcs} PCs and {mt.count_rows()} variants...')
+    logger.info('Running PCA for PC-Relate...')
+    eig, scores, _ = hl.hwe_normalized_pca(pruned_mt.GT, k=10, compute_loadings=False)
+    scores.write(f'{hdfs_dir}/tmp/chd_ukbb.pruned.pca_scores_for_pc_relate.ht',
+                 overwrite=args.overwrite)
 
-    relatedness = hl.pc_relate(call_expr=mt.GT,
-                               min_individual_maf=args.min_individual_maf,
-                               k=args.n_pcs,
-                               min_kinship=args.min_kinship,
-                               statistics='all',
-                               block_size=1024)
+    logger.info(f'Running PC-_Relate...')
+    scores = hl.read_table(f'{hdfs_dir}/tmp/chd_ukbb.pruned.pca_scores.ht')
+    relatedness_ht = hl.pc_relate(call_expr=pruned_mt.GT,
+                                  min_individual_maf=args.min_individual_maf,
+                                  scores_expr=scores[pruned_mt.col_key].scores,
+                                  block_size=4096,
+                                  min_kinship=args.min_kinship,
+                                  statistics='kin2')
 
     # TODO: retrieve maximal independent sample set
 
+    logger.info(f'Writing relatedness table...')
     # Write/export table to file
-    tb = (relatedness
+    tb = (relatedness_ht
           .flatten()
           .key_by('i.s', 'j.s')
           )
@@ -106,19 +108,23 @@ if __name__ == '__main__':
 
     parser.add_argument('--mt_input_path', help='Path to MatrixTable with computed QC metrics', type=str, default=None)
     parser.add_argument('--ht_output_path', help='Output HailTable path with Kinship stats', type=str, default=None)
-    parser.add_argument('--sample_to_keep', help='Text file (one-column, no header) listing the samples to keep',
-                        type=str, default=None)
+    parser.add_argument('--default_reference', help='One of GRCh37 and GRCh38', type=str, default='GRCh38')
+    parser.add_argument('--maf_threshold', help='Exclude variants with maf lower than maf_threshold',
+                        type=float, default=0.001)
+    parser.add_argument('--write_to_file', help='Write results to TSV file', action='store_true')
+
+    # parameters for ld_prune
+    parser.add_argument('--prune_ld', help='Perform LD pruning before PCA (recommended)', action='store_true')
+    parser.add_argument('--r2', help='Squared correlation threshold for LD pruning', type=float, default=0.1)
+
+    # parameters for pca
     parser.add_argument('--n_pcs', help='Number of PCs to be computed', type=int, default=10)
+
+    # parameters for pc_relate
     parser.add_argument('--min_individual_maf', help='Individual specif MAF cutoff used to run pc_relate method',
-                        type=float, default=0.01)
+                        type=float, default=0.05)
     parser.add_argument('--min_kinship', help='Exclude pairs of samples with kinship lower than min_kinship',
                         type=float, default=0.05)
-    parser.add_argument('--maf_threshold', help='Exclude variants with maf lower than maf_threshold',
-                        type=float, default=0.01)
-    parser.add_argument('--ld_pruning', help='Perform LD pruning before PCA (recommended)', action='store_true')
-    parser.add_argument('--r2', help='Squared correlation threshold for LD pruning', type=float, default=0.2)
-    parser.add_argument('--bp_window_size', help='Window size in bps for ld_prune', type=int, default=500000)
-    parser.add_argument('--write_to_file', help='Write results to TSV file', action='store_true')
 
     args = parser.parse_args()
 
--- submit-scripts/nohup_run_relatedness_inference.sh
+++ submit-scripts/nohup_run_relatedness_inference.sh
@@ -2,10 +2,10 @@
 
 nohup python  ../sample_qc/relatedness_inference.py \
                  --mt_input_path 'file:///home/ubuntu/data/hail_data/mts/chd_ukbb_split_v2_09092020.mt' \
-                 --ht_output_path 'hdfs://spark-master:9820/dir/chd_ukbb_samples_relatedness_kinship_24092020.ht' \
-                 --ld_pruning \
+                 --ht_output_path 'hdfs://spark-master:9820/dir/hail_data/chd_ukbb.relatedness_kinship.ht' \
+                 --prune_ld \
                  --write_to_file \
-                 > nohup.log 2>&1 &
+                 > relatedness_inference.log 2>&1 &
 
 
 
