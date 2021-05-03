0c2090f972c76ff3ec737ffbd0f23d272c5dcbca
enriquea <enrique.audain@gmail.com>
2021-05-03 02:27:53 +0200

    maf filter expression

--- utils/expressions.py
+++ utils/expressions.py
@@ -18,4 +18,16 @@ def bi_allelic_expr(t: Union[hl.Table, hl.MatrixTable]) -> hl.expr.BooleanExpres
     return ~t.was_split if "was_split" in t.row else (hl.len(t.alleles) == 2)
 
 
+def af_filter_expr(t: Union[hl.Table, hl.MatrixTable],
+                   af_field: str,
+                   af_cutoff: float = 0.001) -> hl.expr.BooleanExpression:
+    """
+    Returns a boolean expression filtering (rare) sites by allelic frequency.
+    Note: keep sites with missing maf.
 
+    :param t: Input HT or MT
+    :param af_field: Name of the allelic frequency field
+    :param af_cutoff: Cutoff to apply
+    :return: Boolean expression selecting sites with AF <= cutoff
+    """
+    return (t[af_field] <= af_cutoff) | hl.is_missing(t[af_field])
