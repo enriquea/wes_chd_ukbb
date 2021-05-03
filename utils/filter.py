58f8ac0924a8a828c3983a923590a2b611426f4f
enriquea <enrique.audain@gmail.com>
2021-05-03 02:28:46 +0200

    added filter functions

--- utils/filter.py
+++ utils/filter.py
@@ -7,6 +7,14 @@ general-propose filter functions
 from typing import List, Optional, Tuple, Union
 from utils.reference_genome import get_reference_genome
 
+from utils.reference_data import (get_lcr_ht,
+                                  get_segdups_ht,
+                                  get_telomeres_and_centromeres_ht,
+                                  import_cds_intervals_from_gtf)
+
+import functools
+import operator
+
 import hail as hl
 
 
@@ -45,3 +53,72 @@ def filter_genotypes_ab(mt: hl.MatrixTable) -> hl.MatrixTable:
     return mt.filter_entries(filter_condition_ab, keep=True)
 
 
+def filter_to_cds_regions(t: Union[hl.Table, hl.MatrixTable]) -> Union[hl.Table, hl.MatrixTable]:
+
+    cds_intervals = import_cds_intervals_from_gtf(overwrite=False)
+
+    if isinstance(t, hl.MatrixTable):
+        t = t.filter_rows(hl.is_defined(cds_intervals[t.locus]))
+    else:
+        t = t.filter(hl.is_defined(cds_intervals[t.locus]))
+    return t
+
+
+def remove_telomeres_centromes(t: Union[hl.Table, hl.MatrixTable]) -> Union[hl.Table, hl.MatrixTable]:
+
+    tc_intervals = get_telomeres_and_centromeres_ht(overwrite=False)
+
+    if isinstance(t, hl.MatrixTable):
+        t = t.filter_rows(hl.is_missing(tc_intervals[t.locus]))
+    else:
+        t = t.filter(hl.is_missing(tc_intervals[t.locus]))
+    return t
+
+
+# TODO: filter_low_conf_region
+def filter_low_conf_regions(
+        mt: Union[hl.MatrixTable, hl.Table],
+        filter_lcr: bool = True,
+        filter_segdup: bool = True) -> Union[hl.MatrixTable, hl.Table]:
+    """
+    Filters low-confidence regions
+
+    :param mt: MatrixTable or Table to filter
+    :param filter_lcr: Whether to filter LCR regions
+    # :param filter_decoy: Whether to filter decoy regions
+    :param filter_segdup: Whether to filter Segdup regions
+    # :param filter_exome_low_coverage_regions: Whether to filter exome low confidence regions
+    # :param high_conf_regions: Paths to set of high confidence regions to restrict to (union of regions)
+    :return: MatrixTable or Table with low confidence regions removed
+    """
+
+    criteria = []
+    if filter_lcr:
+        lcr = get_lcr_ht()
+        criteria.append(hl.is_missing(lcr[mt.locus]))
+
+    if filter_segdup:
+        segdup = get_segdups_ht()
+        criteria.append(hl.is_missing(segdup[mt.locus]))
+
+    # if filter_decoy:
+    #    decoy = resources.decoy_intervals.ht()
+    #    criteria.append(hl.is_missing(decoy[mt.locus]))
+
+    # if filter_exome_low_coverage_regions:
+    #    high_cov = resources.high_coverage_intervals.ht()
+    #    criteria.append(hl.is_missing(high_cov[mt.locus]))
+
+    # if high_conf_regions is not None:
+    #    for region in high_conf_regions:
+    #        region = hl.import_locus_intervals(region)
+    #        criteria.append(hl.is_defined(region[mt.locus]))
+
+    if criteria:
+        filter_criteria = functools.reduce(operator.iand, criteria)
+        if isinstance(mt, hl.MatrixTable):
+            mt = mt.filter_rows(filter_criteria)
+        else:
+            mt = mt.filter(filter_criteria)
+
+    return mt
