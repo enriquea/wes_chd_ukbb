01e629bce44d75f6c087934cfcccd1ec3b7d8b7c
enriquea <enrique.audain@gmail.com>
2021-06-08 19:01:36 +0200

    fixed path

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -9,21 +9,22 @@ hdfs_dir = 'hdfs://spark-master:9820/dir/hail_data'
 hdfs_checkpoint_dir = 'hdfs://spark-master:9820/checkpoint'
 
 
-def get_mt_data(dataset: str = 'chd_ukbb', part: str = None) -> hl.MatrixTable:
+def get_mt_data(dataset: str = 'chd_ukbb', part: str = None, split: bool = True) -> hl.MatrixTable:
     """
     Return MatrixTable for different version of CHD/UKBB cohort. Multi-allelic sites were split.
 
-    :param dataset: dataset name (Refers just to one ds at the moment, but included to handle multiple ds...)
-    :type part: object
+    :param split: Multi-allelic variants were split
+    :param dataset: dataset name (Refers just to one ds at the moment, but should handle multiple ds)
     :param part: One of:
-                unfiltered: unfiltered MatrixTable with split multi-allelic sites.
+                raw: unfiltered MatrixTable with split multi-allelic sites.
                 filtered_high_callrate: filtered MT for common SNPs (MAF 0.1%), bi-allelic, callrate > 0.99
-    :return MT path
+    :return MT
 
     """
     # TODO: add different MT versions here...
-    if part == 'unfiltered':
-        return hl.read_matrix_table(f'{nfs_dir}/hail_data/mts/{dataset}_split_v2_09092020.mt')
+    if part == 'raw':
+        split = '.split' if split else ''
+        return hl.read_matrix_table(f'{nfs_dir}/hail_data/mts/{dataset}.{part}{split}.mt')
     elif part == 'filtered_high_callrate':
         return hl.read_matrix_table(f'{nfs_dir}/hail_data/mts/{dataset}.high_callrate.common_snp.biallelic.mt')
     else:
@@ -155,7 +156,8 @@ def get_chd_denovo_ht() -> hl.Table:
 def get_variant_qc_ht_path(dataset: str = 'chd_ukbb', part: str = None, split=True) -> str:
     qc_parts = ['vep_vqsr',
                 'hard_filters',
-                'rf_result']
+                'rf_result',
+                'final_qc']
 
     if part not in qc_parts:
         raise DataException(f'Expected part one of: {qc_parts}')
@@ -170,7 +172,7 @@ def get_vep_vqsr_vcf_path() -> str:
 
 def get_vep_annotation_ht() -> hl.Table:
     return hl.read_table(
-        get_variant_qc_ht_path(part='vep')
+        get_variant_qc_ht_path(part='vep_vqsr')
     )
 
 
