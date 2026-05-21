9cc414b2e6a6fd5839593be81285f19bdc6b9072
enriquea <enrique.audain@gmail.com>
2026-05-22 00:01:43 +0200

    Refactor sample_qc/apply_hard_filters.py: modularize into helper functions

--- sample_qc/apply_hard_filters.py
+++ sample_qc/apply_hard_filters.py
@@ -1,3 +1,10 @@
+"""
+Apply hard filters to samples for sample QC.
+
+Filters the input MatrixTable to high-callrate, common, bi-allelic SNPs,
+imputes sex, flags ambiguous sex / aneuploidies, annotates each sample with
+the set of hard filters it fails, and writes the result to a Hail Table.
+"""
 import argparse
 import logging
 
@@ -55,31 +62,29 @@ def make_hard_filters_expr(ht: hl.Table) -> hl.expr.SetExpression:
                             [hl.or_missing(filter_expr, name) for name, filter_expr in hard_filters.items()]))
 
 
-def main(args):
-    hl.init(default_reference=args.default_reference)
+def filter_and_write_qc_mt(exome_cohort: str, overwrite: bool) -> None:
+    """Filter input MT to bi-allelic, high-callrate, common SNPs and write to disk."""
+    # import unfiltered MT
+    mt = get_mt_data(dataset=exome_cohort, part='unfiltered')
 
-    logger.info("Importing data...")
-    # read MT
-    if not args.skip_filter_step:
+    logger.info("Filtering to bi-allelic, high-callrate, common SNPs for sample QC...")
 
-        # import unfiltered MT
-        mt = get_mt_data(dataset=args.exome_cohort, part='unfiltered')
+    mt = mt.filter_rows(bi_allelic_expr(mt) &
+                        hl.is_snp(mt.alleles[0], mt.alleles[1]) &
+                        (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001) &
+                        (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99))
 
-        logger.info("Filtering to bi-allelic, high-callrate, common SNPs for sample QC...")
+    logger.info(f"Writing filtered MT with {mt.count_rows()} variants...")
+    (mt
+     .annotate_cols(callrate=hl.agg.fraction(hl.is_defined(mt.GT)))
+     .naive_coalesce(1000)
+     .write(get_qc_mt_path(part='high_callrate_common_snp_biallelic', split=True),
+            overwrite=overwrite)
+     )
 
-        mt = mt.filter_rows(bi_allelic_expr(mt) &
-                            hl.is_snp(mt.alleles[0], mt.alleles[1]) &
-                            (hl.agg.mean(mt.GT.n_alt_alleles()) / 2 > 0.001) &
-                            (hl.agg.fraction(hl.is_defined(mt.GT)) > 0.99))
 
-        logger.info(f"Writing filtered MT with {mt.count_rows()} variants...")
-        (mt
-         .annotate_cols(callrate=hl.agg.fraction(hl.is_defined(mt.GT)))
-         .naive_coalesce(1000)
-         .write(get_qc_mt_path(part='high_callrate_common_snp_biallelic', split=True),
-                overwrite=args.overwrite)
-         )
-
+def load_qc_mt_with_metadata() -> hl.MatrixTable:
+    """Load QC MatrixTable, unphase it, and annotate columns with sex-chrom coverage metadata."""
     qc_mt = hl.read_matrix_table(get_qc_mt_path(part='high_callrate_common_snp_biallelic', split=True))
 
     # unphase MT. required for imputing sex...
@@ -91,7 +96,11 @@ def main(args):
                )
 
     qc_mt = qc_mt.annotate_cols(**meta_ht[qc_mt.s])
+    return qc_mt
 
+
+def compute_sex_annotations(qc_mt: hl.MatrixTable) -> hl.Table:
+    """Infer sex and annotate ambiguous_sex and sex_aneuploidy flags; return cols Table."""
     logger.info("Inferring sex...")
     qc_ht = annotate_sex(qc_mt,
                          female_threshold=0.5,
@@ -106,7 +115,11 @@ def main(args):
                                                   qc_ht.chrY_normalized_coverage > 0.1))),
                            sex_aneuploidy=(qc_ht.f_stat < 0.4) & hl.is_defined(qc_ht.chrY_normalized_coverage) & (
                                    qc_ht.chrY_normalized_coverage > 0.1))
+    return qc_ht
 
+
+def annotate_hard_filters(qc_ht: hl.Table) -> hl.Table:
+    """Annotate table with hard_filters set and inferred sex label, keyed by sample ID."""
     logger.info("Annotating samples failing hard filters...")
 
     sex_expr = (hl.case()
@@ -117,15 +130,22 @@ def main(args):
 
     qc_ht = qc_ht.annotate(hard_filters=make_hard_filters_expr(qc_ht),
                            sex=sex_expr).key_by('s')
+    return qc_ht
 
+
+def write_qc_ht(qc_ht: hl.Table, overwrite: bool, write_to_file: bool) -> None:
+    """Describe, write hard-filters Table, and optionally export as BGZ-compressed TSV."""
     qc_ht.describe()
 
     qc_ht.write(get_sample_qc_ht_path(part='hard_filters'),
-                overwrite=args.overwrite)
+                overwrite=overwrite)
 
-    if args.write_to_file:
+    if write_to_file:
         qc_ht.export(get_sample_qc_ht_path(part='hard_filters') + '.tsv.bgz')
 
+
+def log_filter_counts() -> None:
+    """Read back the hard-filters Table and log pre/post-filter sample counts."""
     # Export annotations to make rank list for relatedness (in final sample QC)
 
     # Check numbers:
@@ -134,6 +154,25 @@ def main(args):
     checkpoint1a = qc_ht.aggregate(hl.agg.count_where(hl.len(qc_ht['hard_filters']) == 0))
     logger.info('{} samples found before filtering'.format(sample_count))
     logger.info('{} samples found after checkpoint 1a (hard filters)'.format(checkpoint1a))
+
+
+def main(args):
+    hl.init(default_reference=args.default_reference)
+
+    logger.info("Importing data...")
+    # read MT
+    if not args.skip_filter_step:
+        filter_and_write_qc_mt(exome_cohort=args.exome_cohort, overwrite=args.overwrite)
+
+    qc_mt = load_qc_mt_with_metadata()
+
+    qc_ht = compute_sex_annotations(qc_mt)
+
+    qc_ht = annotate_hard_filters(qc_ht)
+
+    write_qc_ht(qc_ht, overwrite=args.overwrite, write_to_file=args.write_to_file)
+
+    log_filter_counts()
 
 
 if __name__ == '__main__':
