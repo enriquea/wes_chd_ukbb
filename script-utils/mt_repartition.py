9861f88f8a1a71ee94a9911bcdea317105c4bcad
enriquea <enrique.audain@gmail.com>
2026-05-22 00:47:31 +0200

    Refactor script-utils/mt_repartition.py: wrap logic in main with __main__ guard

--- script-utils/mt_repartition.py
+++ script-utils/mt_repartition.py
@@ -1,20 +1,27 @@
-# Decrease the number of original partitions in the MatrixTable
+"""Decrease the number of original partitions in the MatrixTable."""
 
 import hail as hl
 
 from utils.config import NFS_DIR
 
-# Init Hail on cluster
-hl.init(default_reference='GRCh38')
 
-# read MT
-mt = hl.read_matrix_table(f'{NFS_DIR}/hail_data/mts/chd_ukbb_split_09092020.mt')
+def main() -> None:
+    """Read the CHD-UKBB MatrixTable, downsample partitions, and write the result."""
+    # Init Hail on cluster
+    hl.init(default_reference='GRCh38')
 
-# downsample number of partitions
-mt = mt.repartition(n_partitions=2000,
-                    shuffle=False)
-# write new MT
-mt.write(f'{NFS_DIR}/hail_data/mts/chd_ukbb_split_v2_09092020.mt')
+    # read MT
+    mt = hl.read_matrix_table(f'{NFS_DIR}/hail_data/mts/chd_ukbb_split_09092020.mt')
 
-# stop Hail
-hl.stop()
+    # downsample number of partitions
+    mt = mt.repartition(n_partitions=2000,
+                        shuffle=False)
+    # write new MT
+    mt.write(f'{NFS_DIR}/hail_data/mts/chd_ukbb_split_v2_09092020.mt')
+
+    # stop Hail
+    hl.stop()
+
+
+if __name__ == '__main__':
+    main()
