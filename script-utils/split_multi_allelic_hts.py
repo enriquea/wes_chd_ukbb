c55042b4f04939902246c9fad463cfb7f284238d
enriquea <enrique.audain@gmail.com>
2026-05-22 00:47:31 +0200

    Refactor script-utils/split_multi_allelic_hts.py: wrap logic in main with __main__ guard

--- script-utils/split_multi_allelic_hts.py
+++ script-utils/split_multi_allelic_hts.py
@@ -1,12 +1,6 @@
-import argparse
-import hail as hl
-import os
-
-from utils.config import NFS_DIR
-
 """
 Simple script to generate a fully bi-allelic MatrixTable
-from a MT contating multi-allelic variants. 
+from a MT contating multi-allelic variants.
 
 All Call fields are also split/downcoded.
 
@@ -14,20 +8,31 @@ See https://hail.is/docs/0.2/methods/genetics.html#hail.methods.split_multi_hts
 for details.
 """
 
-# init Hail
-hl.init(default_reference='GRCh38')
+import hail as hl
 
-# path to MatrixTable
-mt_path = f'{NFS_DIR}/hail_data/mts/chd_ukbb_08092020.mt'
+from utils.config import NFS_DIR
 
-# read matrix
-mt = hl.read_matrix_table(mt_path)
 
-# split multi-allelic variant into bi-allelic. It will also downcode the genotypes.
-mt = hl.split_multi_hts(mt)
+def main() -> None:
+    """Split multi-allelic variants in the CHD-UKBB MatrixTable into bi-allelic sites."""
+    # init Hail
+    hl.init(default_reference='GRCh38')
 
-# write MT to disk
-mt.write(f"{NFS_DIR}/hail_data/mts/chd_ukbb_split_09092020.mt")
+    # path to MatrixTable
+    mt_path = f'{NFS_DIR}/hail_data/mts/chd_ukbb_08092020.mt'
 
-# stop Hail
-hl.stop()
+    # read matrix
+    mt = hl.read_matrix_table(mt_path)
+
+    # split multi-allelic variant into bi-allelic. It will also downcode the genotypes.
+    mt = hl.split_multi_hts(mt)
+
+    # write MT to disk
+    mt.write(f"{NFS_DIR}/hail_data/mts/chd_ukbb_split_09092020.mt")
+
+    # stop Hail
+    hl.stop()
+
+
+if __name__ == '__main__':
+    main()
